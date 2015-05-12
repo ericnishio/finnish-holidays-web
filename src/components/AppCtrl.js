d5ec06348f4907c836547c704efa9e5332f3c1e8
Eric Nishio <eric@self-learner.com>
2015-05-13 00:06:35 +0300

    Add fade animation when navigating between holidays

--- src/components/AppCtrl.js
+++ src/components/AppCtrl.js
@@ -5,7 +5,7 @@ var FinnishHolidays = require('finnish-holidays-js');
 var KEY_LEFT = 37;
 var KEY_RIGHT = 39;
 
-app.controller('AppCtrl', function($log, $scope, utils) {
+app.controller('AppCtrl', function($timeout, $log, $scope, utils) {
   var currentHoliday;
   var currentDate = moment();
   var currentIndex = -1;
@@ -24,6 +24,7 @@ app.controller('AppCtrl', function($log, $scope, utils) {
         currentHoliday = h;
         currentDate = moment([h.year, h.month - 1, h.day]).add(1, 'days');
         currentIndex = i;
+        setImageClass();
       }
     });
 
@@ -39,6 +40,11 @@ app.controller('AppCtrl', function($log, $scope, utils) {
     if (holidays.length > 0 && currentIndex < holidays.length - 1) {
       currentHoliday = holidays[currentIndex + 1];
       currentIndex += 1;
+      hideImage();
+
+      $timeout(function() {
+        setImageClass();
+      }, 1000);
     } else {
       nextMonth();
       gotoNext();
@@ -49,8 +55,13 @@ app.controller('AppCtrl', function($log, $scope, utils) {
     var holidays = getHolidays();
 
     if (holidays.length > 0 && currentIndex > 0) {
+      hideImage();
       currentHoliday = holidays[currentIndex - 1];
       currentIndex -= 1;
+
+      $timeout(function() {
+        setImageClass();
+      }, 1000);
     } else {
       previousMonth();
       gotoPrevious();
@@ -71,6 +82,71 @@ app.controller('AppCtrl', function($log, $scope, utils) {
     var m = currentDate.get('month') + 1;
     var y = currentDate.get('year');
     return FinnishHolidays.month(m, y);
+  }
+
+  function setImageClass(holiday) {
+    $scope.imageClass = getImageClass(currentHoliday.description);
+    showImage();
+  }
+
+  function hideImage() {
+    $scope.imageVisibleClass = '';
+  }
+
+  function showImage() {
+    $scope.imageVisibleClass = 'background-image--visible';
+  }
+
+  function getImageClass(day) {
+    switch (day) {
+      case "All Saint's Day":
+        return 'background-image--all-saints-day';
+
+      case 'Ascension Day':
+        return 'background-image--ascension-day';
+
+      case 'Christmas Day':
+        return 'background-image--christmas-day';
+
+      case 'Christmas Eve':
+        return 'background-image--christmas-eve';
+
+      case 'Easter Monday':
+        return 'background-image--easter-monday';
+
+      case 'Easter Sunday':
+        return 'background-image--easter-sunday';
+
+      case 'Epiphany':
+        return 'background-image--epiphany';
+
+      case 'Good Friday':
+        return 'background-image--good-friday';
+
+      case 'Independence Day':
+        return 'background-image--independence-day';
+
+      case 'May Day':
+        return 'background-image--may-day';
+
+      case 'Midsummer Day':
+        return 'background-image--midsummer-day';
+
+      case 'Midsummer Eve':
+        return 'background-image--midsummer-eve';
+
+      case "New Year's Day":
+        return 'background-image--new-year';
+
+      case 'Pentecost':
+        return 'background-image--pentecost';
+
+      case "St. Stephen's Day":
+        return 'background-image--st-stephens-day';
+
+      case 'Midsummer Eve':
+        return 'background-image--midsummer-eve';
+    }
   }
 
   $scope.getHoliday = function() {
