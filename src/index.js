5e78bff3f713cf68168ecd8f0f087f50edbc9d34
Eric Nishio <ericnishio@protonmail.com>
2018-05-31 15:59:46 +0300

    Replace babel-polyfill with core-js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {injectGlobal} from 'styled-components'
-import 'babel-polyfill'
+import 'core-js/fn/array/find'
 
 import App from './bootstrap/App'
 import registerServiceWorker from './bootstrap/registerServiceWorker'
