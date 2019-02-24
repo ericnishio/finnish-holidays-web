4e1141f5167627e23acd44466ce8af2e1562fda5
Eric Nishio <ericnishio@protonmail.com>
2019-02-24 02:12:41 +0200

    Updates

---
+++ precache-manifest.92363740368e6440b0f111e81f09b979.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "38d1c12f9a58abca324f",
+    "url": "/static/css/main.88e67447.chunk.css"
+  },
+  {
+    "revision": "38d1c12f9a58abca324f",
+    "url": "/static/js/main.38d1c12f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "e722a902788a483d9ad5",
+    "url": "/static/js/2.e722a902.chunk.js"
+  },
+  {
+    "revision": "aa8c5be7c9ee3438595bcb2da69a438c",
+    "url": "/static/media/logo.aa8c5be7.png"
+  },
+  {
+    "revision": "d29587227d946b1bf2c9f49fbef09fc6",
+    "url": "/static/media/futura-medium.d2958722.woff2"
+  },
+  {
+    "revision": "96278410331aa8f8325c0203e2567e2b",
+    "url": "/static/media/futura-medium.96278410.woff"
+  },
+  {
+    "revision": "423537caf9b6584f4cf07db8ac1bb592",
+    "url": "/index.html"
+  }
+];
